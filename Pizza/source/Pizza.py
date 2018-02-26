d2517762edc60604e457df7a070ef8820580672b
Matthias64 <matthias.larre@gmail.com>
2018-02-27 00:34:50 +0100

    Guillotine Solution

--- Pizza/source/Pizza.py
+++ Pizza/source/Pizza.py
@@ -1,3 +1,7 @@
+import numpy as np
+import tqdm
+
+
 class Pizza:
     def __init__(self, grid, l, h):
         self.n_rows = len(grid)
@@ -11,7 +15,7 @@ class Pizza:
         s = "Pizza Size = {} rows * {} cols\n".format(self.n_rows, self.n_cols)
         s += "Min Number of each ingredient per slice = {}\n".format(self.min_ing_number)
         s += "Max Size of each slice = {}\n".format(self.max_size)
-        s += "Pizza\n{}".format("\n".join(self.grid))
+        s += "Pizza\n{}".format(str(self.grid))
         return s
 
     def get_output(self):
@@ -30,27 +34,34 @@ class Pizza:
                         for i in xrange(self.n_rows)
                         for j in xrange(self.n_cols)
                         for k in xrange(i + 1, self.n_rows + 1)
-                        for l in xrange(j + 1, self.n_cols + 1)]
+                        for l in xrange(j + 1, self.n_cols + 1)
+                        if (k - i) * (l - j) >= 2 * self.min_ing_number]
 
         # pizza_slices = [pizza_slice for pizza_slice in pizza_slices if pizza_slice.slice_size >= min_slice_size]
         pizza_slices.sort(key=lambda (m, n, o, p): (o - m) * (p - n))
 
         slice_score = {}
         slice_cut = {}
-        for (i, j, k, l) in pizza_slices:
+        for (i, j, k, l) in tqdm.tqdm(pizza_slices):
             slice_score[(i, j, k, l)] = 0
             slice_cut[(i, j, k, l)] = None
-            if not self.is_slice_valid(i, j, k, l):
+            if not self.is_slice_content_valid(i, j, k, l):
                 continue
-            if (k - i) * (l - j) <= self.max_size:
+            if self.is_slice_size_valid(i, j, k, l):
                 slice_score[(i, j, k, l)] = (k - i) * (l - j)
             for vertical_cut in xrange(1, l - j):
-                temp_score = slice_score[(i, j, k, j + vertical_cut)] + slice_score[(i, j + vertical_cut, k, l)]
+                temp_score = slice_score[(i, j, k, j + vertical_cut)] \
+                    if (i, j, k, j + vertical_cut) in slice_score else 0
+                temp_score += slice_score[(i, j + vertical_cut, k, l)] \
+                    if (i, j + vertical_cut, k, l) in slice_score else 0
                 if temp_score > slice_score[(i, j, k, l)]:
                     slice_score[(i, j, k, l)] = temp_score
                     slice_cut[(i, j, k, l)] = vertical_cut
             for horizontal_cut in xrange(1, k - i):
-                temp_score = slice_score[(i, j, i + horizontal_cut, l)] + slice_score[(i + horizontal_cut, j, k, l)]
+                temp_score = slice_score[(i, j, i + horizontal_cut, l)] \
+                    if (i, j, i + horizontal_cut, l) in slice_score else 0
+                temp_score += slice_score[(i + horizontal_cut, j, k, l)] \
+                    if (i + horizontal_cut, j, k, l) in slice_score else 0
                 if temp_score > slice_score[(i, j, k, l)]:
                     slice_score[(i, j, k, l)] = temp_score
                     slice_cut[(i, j, k, l)] = -horizontal_cut
@@ -69,7 +80,9 @@ class Pizza:
                 stack.append((i, j, i - cut, l))
                 stack.append((i - cut, j, k, l))
 
-    def is_slice_valid(self, i, j, k, l):
-        # print i,j,k,l
-        n_tomato = sum(self.grid[i + m][j:l].count("T") for m in xrange(k - i))
+    def is_slice_content_valid(self, i, j, k, l):
+        n_tomato = np.sum(self.grid[i:k, j:l])
         return (k - i) * (l - j) - self.min_ing_number >= n_tomato >= self.min_ing_number
+
+    def is_slice_size_valid(self, i, j, k, l):
+        return (k - i) * (l - j) <= self.max_size
--- Pizza/source/PizzaParser.py
+++ Pizza/source/PizzaParser.py
@@ -1,10 +1,9 @@
 from Pizza import Pizza
+import numpy as np
 
 
 def pizza_parser(file_name):
     with open(file_name, "r") as f:
         r, c, l, h = map(int, f.readline().strip().split())
-        grid = []
-        for i in xrange(r):
-            grid.append(f.readline().strip())
+        grid = np.array([list(map(lambda x: 1 if x == 'T' else 0, f.readline().strip())) for _ in xrange(r)])
         return Pizza(grid, l, h)
