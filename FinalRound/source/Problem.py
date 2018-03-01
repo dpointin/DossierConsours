f6d38e4fa97ba30da5a75d121ba0e71f1421f8c7
dpointin <damien.pointin>
2018-03-01 19:10:16 +0100

    problem ok

--- FinalRound/source/Problem.py
+++ FinalRound/source/Problem.py
@@ -1,17 +1,15 @@
 class Problem:
-    def __init__(self, grid, l, h):
-        self.n_rows = len(grid)
-        self.n_cols = len(grid[0])
-        self.grid = grid
-        self.min_ing_number = l
-        self.max_size = h
-        self.slices = []
+    def __init__(self, grille, list_vehicle, list_rides, T, B):
+        self.grid = grille
+        self.list_vehicle=list_vehicle
+        self.list_rides = list_rides
+        self.time = T
+        self.bonus = B
+
+
 
     def __str__(self):
-        s = "Pizza Size = {} rows * {} cols\n".format(self.n_rows, self.n_cols)
-        s += "Min Number of each ingredient per slice = {}\n".format(self.min_ing_number)
-        s += "Max Size of each slice = {}\n".format(self.max_size)
-        s += "Pizza\n{}".format("\n".join(self.grid))
+        s = "\n".join(str(r) for r in self.list_vehicle)
         return s
 
     def get_output(self):
