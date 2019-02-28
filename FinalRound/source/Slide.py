a95d49c0d433a08390e191a82b6b0aad404deeea
Pierre <pierre.gautier@insa-rouen.fr>
2019-02-28 19:30:03 +0100

    test

--- FinalRound/source/Slide.py
+++ FinalRound/source/Slide.py
@@ -7,8 +7,11 @@ class Slide:
         return "slide {}\n".format(str(self.photos))
 
     def score(self, autre_slide):
-        common_tag = self.tags.intersection(autre_slide.tags)
-        tag_only_s1 = self.tags.difference(autre_slide.tags)
-        tag_only_s2 = autre_slide.tags.difference(self.tags)
+        common_tag = len(self.tags.intersection(autre_slide.tags))
+        tag_only_s1 = len(self.tags.difference(autre_slide.tags))
+        tag_only_s2 = len(autre_slide.tags.difference(self.tags))
         score = min(common_tag, tag_only_s1, tag_only_s2)
         return score
+
+    def score_ideal(self, autre_slide):
+        return (len(self.tags) + len(autre_slide.tags))/4
