c4fb182138e8b21628e705a78a0e5507f31da78f
Matthias64 <matthias.larre@gmail.com>
2019-02-28 20:37:20 +0100

    a

--- FinalRound/source/Problem.py
+++ FinalRound/source/Problem.py
@@ -9,13 +9,13 @@ class Problem:
 
     def __str__(self):
         s = "Slides available {}\n".format("\n".join(str(slide) for slide in self.slides))
-        #s += "Slideshow \n{}\n".format("\n".join(str(slide) for slide in self.slideshow))
+        # s += "Slideshow \n{}\n".format("\n".join(str(slide) for slide in self.slideshow))
         return s
 
     def get_output(self):
         s = str(len(self.slideshow)) + "\n"
         for slide in self.slideshow:
-            s+= " ".join(str(photo.id) for photo in slide.photos) + "\n"
+            s += " ".join(str(photo.id) for photo in slide.photos) + "\n"
         return s
 
     def slide_creator(self):
@@ -33,25 +33,44 @@ class Problem:
         return slides
 
     def solve(self):
-        #Clean slides with one tag
-        #self.slides = [slide for slide in self.slides if slide.tags < 1]
+        # Clean slides with one tag
+        # self.slides = [slide for slide in self.slides if slide.tags < 1]
         print "****", self
         curr_slide = self.slides[0]
         self.slideshow.append(curr_slide)
         self.slides.remove(curr_slide)
         i = 0
         s = len(self.slides)
-        while self.slides :
-            if i%100 == 0 :
-                print "{} loops done over {}".format(i,s)
+        while self.slides:
+            if i % 100 == 0:
+                print "{} loops done over {}".format(i, s)
             max_score = -1
             max_slide = None
-            for slide in self.slides :
+            for slide in self.slides:
                 slide_score = slide.score(curr_slide)
-                if slide_score > max_score :
+                if slide_score > max_score:
                     max_score = slide_score
                     max_slide = slide
             self.slideshow.append(max_slide)
             self.slides.remove(max_slide)
             curr_slide = max_slide
-            i+=1
+            i += 1
+
+    def solve_example_b(self):
+        self.slides.sort(key=lambda x: len(x.tags))
+        curr_slide = self.slides[0]
+        i = 0
+        s = len(self.slides)
+        while self.slides:
+            if i % 100 == 0:
+                print "{} loops done over {}".format(i, s)
+            for slide in self.slides:
+                slide_score = slide.score(curr_slide)
+                if slide_score > 0:
+                    self.slideshow.append(slide)
+                    self.slideshow.remove(slide)
+                    curr_slide = slide
+                    break
+            else:
+                break
+            i += 1
