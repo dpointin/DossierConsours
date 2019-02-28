446ae3f74a15c492d7dfdf32ad88b027e5c97797
Pierre <pierre.gautier@insa-rouen.fr>
2019-02-28 19:13:32 +0100

    test

--- FinalRound/source/Slide.py
+++ FinalRound/source/Slide.py
@@ -1,10 +1,10 @@
 class Slide:
-        def __init__(self, photos):
-            self.photos=photos
-            self.tags=set.union(*[photo.tags for photo in photos])
+    def __init__(self, photos):
+        self.photos=photos
+        self.tags=set.union(*[photo.tags for photo in photos])
 
-        def __str__(self):
-            return "slide {}\n".format(str(self.photos))
+    def __str__(self):
+        return "slide {}\n".format(str(self.photos))
 
-        def score(self, autre_slide):
-            score = min()
+    def score(self, autre_slide):
+        score = min()
